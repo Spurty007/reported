699206c3441755ba8e59e7d5cb606d3a1c1dada4
Adrian Lewis <spurty@gmail.com>
2023-01-03 22:03:37 -0500

    How to re-render page?

--- www/js/data.js
+++ www/js/data.js
@@ -1,3 +1,13 @@
+function Data() {
+    let update="";
+    let jdata = [['Town','#T1'],['Team','#T2'],['Score','#S1'],['Score','#S2']];
+    jdata.forEach((jdata) => { 
+        let chunk='<script>$(function() {var $dropdown = $(this); $.getJSON("data/'+jdata[0]+'.json", function(data) {var key = \''+jdata[0]+'\'; var vals = []; vals = data.'+jdata[0]+'.split(","); var $'+jdata[0]+' = $("'+jdata[1]+'");$'+jdata[0]+'.empty();$.each(vals, function(index, value) {$'+jdata[0]+'.append("<option>" + value + "<\/option>");});});}</script>';
+        update=update+chunk;
+    });
+    $( "#Refactor" ).replaceWith( update ); // page renders before update
+}
+
 function Validate() {
     if (document.forms["ssrt"]["T1"].value == "") { alert("Town name must be filled out"); return false; }
     if (document.forms["ssrt"]["T2"].value == "") { alert("Team name must be filled out"); return false; }
@@ -8,17 +18,4 @@ function Validate() {
     var logitime=date.getHours() + ':' + date.getMinutes() + ':' +date.getSeconds();
     var message='{"Time":"'+logitime+'","Day":"'+ logiday +'","' + document.forms["ssrt"]["T2"].value + '":"' + document.forms["ssrt"]["S2"].value + '","' + document.forms["ssrt"]["T1"].value + '":"' + document.forms["ssrt"]["S1"].value + '"}';
     document.getElementById("posted").innerHTML = message;
-}
-
-function Data() {
-    let update="";
-    let jdata = [['Town','#T1'],['Team','#T2'],['Score','#S1'],['Score','#S2']];
-    jdata.forEach((jdata) => { 
-        let chunk='<script>$(function() {var $dropdown = $(this); $.getJSON("data/'+jdata[0]+'.json", function(data) {var key = \''+jdata[0]+'\'; var vals = []; vals = data.'+jdata[0]+'.split(","); var $'+jdata[0]+' = $("'+jdata[1]+'");$'+jdata[0]+'.empty();$.each(vals, function(index, value) {$'+jdata[0]+'.append("<option>" + value + "<\/option>");});});}</script>';
-        update=update+chunk;
-    });
-    console.log(update);
-    /*
-    document.getElementById("Refactor").innerHTML = update;
-    */
 }
