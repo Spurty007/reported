c59ef4217586fb7924a0dbb5e008e8667f6242b2
Adrian Lewis <spurty@gmail.com>
2023-01-10 22:03:35 -0500

    Maximize space for user to select

--- www/js/data.js
+++ www/js/data.js
@@ -1,27 +1,33 @@
 function Data() {
     let update="";
-    let jdata = [['Town','#T1'],['Team','#T2'],['Score','#S1'],['Score','#S2']];
+    let jdata = [['Team','#T1'],['Town','#T2'],['Score','#S1'],['Score','#S2']];
     jdata.forEach((jdata) => { 
-        let chunk='<script>$(function() {var $dropdown = $(this); $.getJSON("data/'+jdata[0]+'.json", function(data) {var key = \''+jdata[0]+'\'; var vals = []; vals = data.'+jdata[0]+'.split(","); var $'+jdata[0]+' = $("'+jdata[1]+'");$'+jdata[0]+'.empty();$.each(vals, function(index, value) {$'+jdata[0]+'.append("<option>" + value + "<\/option>");});});})</script>';
+        let chunk='<script>$(function() {var $dropdown = $(this); $.getJSON("data/' + 
+            jdata[0] + '.json", function(data) {var key = \'' + 
+            jdata[0] + '\'; var vals = []; vals = data.' +
+            jdata[0] + '.split(","); var $' + 
+            jdata[0] + ' = $("' + jdata[1] + '");$' + 
+            jdata[0] + '.empty();$.each(vals, function(index, value) {$' + 
+            jdata[0] + '.append("<option>" + value + "<\/option>");});});})</script>';
         update=update+chunk;
     });
     $("#Refactor").replaceWith(update);
 }
 
 function Validate() {
-    if (document.forms["ssrt"]["T1"].value == "") { alert("Town name must be filled out"); return false; }
-    if (document.forms["ssrt"]["T2"].value == "") { alert("Team name must be filled out"); return false; }
-    if (document.forms["ssrt"]["S1"].value == "") { alert("Score for Opponent not selected"); return false; }
-    if (document.forms["ssrt"]["S2"].value == "") { alert("Score for Town not selected"); return false; }
+    if (document.forms["ssrt"]["T1"].value == "") { alert("Team name must be filled out"); return false; }
+    if (document.forms["ssrt"]["T2"].value == "") { alert("Town name must be filled out"); return false; }
+    if (document.forms["ssrt"]["S1"].value == "") { alert("Score for Town not selected"); return false; }
+    if (document.forms["ssrt"]["S2"].value == "") { alert("Score for Opponent not selected"); return false; }
     var t1=document.forms["ssrt"]["T1"].value;
     var t2=document.forms["ssrt"]["T2"].value;
     var s1=document.forms["ssrt"]["S1"].value;
     var s2=document.forms["ssrt"]["S2"].value;
     var obj={};
-    obj["Subject"] = t2 + " vs " + t1;
-    obj["Report"] = t2 + ":" + s2 + "\n" + t1 + ":"+  s1;
+    obj["Subject"] = t1 + " vs " + t2;
+    obj["Report"] = t1 + ":" + s1 + "\n" + t2 + ":"+  s2;
     document.getElementById("posted").innerHTML = "<pre>Report:\n" + 
-        t2 + ":" + s2 + " vs " + t1 + ":" +  s1 + 
+        t1 + ":" + s1 + " vs " + t2 + ":" +  s2 + 
         "\nwas successfully mailed\nThank you!</pre>";
     SendToStatistician(obj);
 }
