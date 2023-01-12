6002422bb39c8701ead5c2fe9ecd597886ba2102
Adrian Lewis <spurty@gmail.com>
2023-01-12 09:03:48 -0500

    Clean up looks

--- www/post.php
+++ www/post.php
@@ -1,7 +1,13 @@
 <?php
     $request = file_get_contents("php://input");
     $params = json_decode($request);
+    $actual_link = dirname('https://'.$_SERVER['HTTP_HOST'].$_SERVER['PHP_SELF']);
     $subject=$params->Subject;
-    $report=$params->Report;
-    mail('YOUREMAILADDRESSHERE', $subject, $report );
+    $report=$params->Report . "\r\n\r\n\r\n\r\nSource: " . $actual_link;
+    $headers = array(
+        'From' => 'Score reporter <YOUREMAILADDRESSHERE>',
+        'Reply-To' => 'YOUREMAILADDRESSHERE',
+        'X-Mailer' => 'PHP/' . phpversion()
+    );
+    mail('YOUREMAILADDRESSHERE', $subject, $report, $headers );
 ?>
